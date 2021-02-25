8526e2f8b86e853532d39a640ab433012b18d550
Chuck Swanberg <cswanberg@mad-swan.com>
2021-02-24 20:16:55 -0500

    tweak

--- src/python/splitgraph/errors.py
+++ src/python/splitgraph/errors.py
@@ -1,8 +1,8 @@
 import typing
 from pandas_schema.validation_warning import ValidationWarning
-class SchemaValidationError(Exception):
 
-   def __init__(self, errors: typing.List[ValidationWarning], message="One or more validations failed"):
+class SchemaValidationError(Exception):
+    def __init__(self, errors: typing.List[ValidationWarning], message="One or more validations failed"):
         self.errors = errors
         self.message = message
         super().__init__(self.message)
