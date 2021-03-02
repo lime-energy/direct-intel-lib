97bc336a9222d4eb5421a47acb6a52075812003b
Chuck Swanberg <cswanberg@mad-swan.com>
2021-03-02 10:57:29 -0500

    fix: add nullserializer

---
+++ src/python/prefect/engine/serializers.py
@@ -0,0 +1,10 @@
+from prefect.engine.serializers import Serializer
+
+class NullSerializer(Serializer):
+    """A Serializer does nothing"""
+
+    def serialize(self, value: Any) -> bytes:
+        return value
+
+    def deserialize(self, value: bytes) -> Any:
+        return value
